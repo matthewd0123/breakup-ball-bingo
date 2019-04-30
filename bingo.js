e0fe6ea008688599b1fabc884ff0a65cd4353db3
Roann Yanes <ryanes@tecture.com>
2019-04-29 20:33:07 -0400

    trying to fix unclick

--- bingo.js
+++ bingo.js
@@ -12,11 +12,10 @@ function markSquare(cell) {
     var currentSquare = document.getElementById(cell);
     if (currentSquare.style.backgroundColor == rgb(218, 165, 32))
         currentSquare.style.backgroundColor = "white";
-        //currentSquare.style.color = rgb(33, 37, 41);
     else
         currentSquare.style.backgroundColor = rgb(218, 165, 32);
-        currentSquare.style.color = "white";
-        currentSquare.style.borderColor = "white";
+        currentSquare.style.color = "white"
+        currentSquare.style.borderColor = "white"
     return;
 }
 
